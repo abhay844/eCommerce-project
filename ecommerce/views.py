301b9530cde7ebc3755c2a073c826a574bf13fd6
Abhay_844 <abhay.katiyar@nextscm.com>
2019-06-26 23:34:31 +0530

    Added the login and register user feature.

--- ecommerce/views.py
+++ ecommerce/views.py
@@ -1,12 +1,18 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
-
+from .forms import ContactForm, LoginForm, RegisterForm
+from django.views.decorators.csrf import csrf_exempt
+from django.contrib.auth import authenticate, login, get_user_model
 
 def home_page(request):
     context = {
-        "title":"Hello This is Django context",
+        "title":"Home page",
         "content":"Welcome to Home page"
     }
+
+    if request.user.is_authenticated:
+        context["premium_content"] = "YEAHH You are logged in"
+        
     return render(request,"home_page.html",context)
 
 def about_page(request):
@@ -16,41 +22,58 @@ def about_page(request):
     }
     return render(request,"home_page.html",context)
 
+
 def contact_page(request):
+    contact_form_obj = ContactForm(request.POST or None)
     context = {
         "title":"Contact page",
-        "content":"Welcome to Contact page"
+        "content":"Welcome to Contact page",
+        "form":contact_form_obj
     }
-    if request.method == "POST":
-        print(request.POST.get("full_name"))
-        print(request.POST.get("email"))
-        print(request.POST.get("content"))
+    if contact_form_obj.is_valid():
+        print(contact_form_obj.cleaned_data)
+        contact_form_obj
+
     return render(request,"contact/view.html",context)
 
-def home_page_old(request):
-    html_ = """
-            <!doctype html>
-            <html lang="en">
-            <head>
-                <!-- Required meta tags -->
-                <meta charset="utf-8">
-                <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
-                <!-- Bootstrap CSS -->
-                <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+def login_page(request):
+    form = LoginForm(request.POST or None)
 
-                <title>Hello, world!</title>
-            </head>
-            <body>
-               <div class="text-center">
-                <h1>Hello, world!</h1>
-                </div>
-                <!-- Optional JavaScript -->
-                <!-- jQuery first, then Popper.js, then Bootstrap JS -->
-                <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
-                <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
-                <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
-            </body>
-            </html>
-            """
-    return HttpResponse(html_)
+    context = {
+        "form":form
+    }
+    # print(request.user.is_authenticated)
+    context = {
+        "form":form
+    }
+    if form.is_valid():
+        
+        username = form.cleaned_data.get("username")
+        password = form.cleaned_data.get("password")
+        user = authenticate(request, username=username, password=password)
+        print("user ", user)
+        if user is not None:
+            login(request, user)
+            
+            return redirect("/login")
+
+    return render(request,"auth/login.html",context)
+
+User = get_user_model()
+def register_page(request):
+    form = RegisterForm(request.POST or None)
+
+    context = {
+        "form":form
+    }
+
+    if form.is_valid():
+        username = form.cleaned_data.get("username")
+        email  = form.cleaned_data.get("email")
+        password = form.cleaned_data.get("password")
+        new_user = User.objects.create_user(username, email, password)
+        print(new_user)
+        print(form.cleaned_data)
+
+    return render(request, "auth/register.html", context)
